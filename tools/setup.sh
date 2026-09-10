313b8c1d3fab096a3272e90d1d47ee4e565ffb71
July-h5kf3 <2312810@mail.nankai.edu.cn>
2026-09-10 16:00:50 +0800

    Revert CI and installer overrides pending administrator guidance

--- tools/setup.sh
+++ tools/setup.sh
@@ -33,11 +33,7 @@ VLLM_VERSION="${VLLM_VERSION:-0.20.2}"
 TORCH_BACKEND="${TORCH_BACKEND:-auto}"
 
 FLAGOS_PYPI="${FLAGOS_PYPI:-https://resource.flagos.net/repository/flagos-pypi-hosted/simple}"
-if [[ "${FLAGGEMS_VENDOR:-${DNN_VENDOR:-}}" == "thead" ]]; then
-  FLAGTREE_VERSION="${FLAGTREE_VERSION:-0.6.2a2+ppu3.6}"
-else
-  FLAGTREE_VERSION="${FLAGTREE_VERSION:-0.6.0}"
-fi
+FLAGTREE_VERSION="${FLAGTREE_VERSION:-0.6.0}"
 
 # Whether to replace Triton with FlagTree.
 # For temporary container validation, you can run:
